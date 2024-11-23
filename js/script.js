b7ac1b4fa3c84534ec87b2aef1f3c88d7c3c544b
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-23 15:49:36 +0500

    adsdasda

--- js/script.js
+++ js/script.js
@@ -853,3 +853,12 @@
 // fruits[fruits.length-4] = "Lemon";
 // // fruits[fruits.length] = "Kiwi";
 // console.log(fruits);
+
+
+const person = [];
+person[0] = "John";
+person[1] = "Doe";
+person[2] = 46;
+console.log(person.length);
+   // Will return 3
+person[0]; 

30a55ed37833262005ea61e407672cd9ca24c8a9
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-25 15:52:59 +0500

    dsadads

--- js/script.js
+++ js/script.js
@@ -882,3 +882,13 @@
 
 // const fruits = 15;
 // console.log(Array.isArray(fruits));
+
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
+
+

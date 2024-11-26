c5dedc1ba07c941276a87f61dcc9e27b0fe10ddf
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-26 15:26:20 +0500

    add

--- js/script.js
+++ js/script.js
@@ -948,6 +948,14 @@
 //     console.log(i * i);
 // }
 
-for (let i = 5; i <= 15; i+=5) {
-    console.log(i);
-}
+// for (let i = 5; i <= 15; i+=5) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = '';
+//     for (let j = 1; j <= i; j++) {
+//         row += '*';
+//     }
+//     console.log(row);
+// }

82d6109eab233f0b27d374548b0bf9acdb30fc04
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-03 14:59:15 +0500

    git add .

--- js/script.js
+++ js/script.js
@@ -12,3 +12,69 @@
 //     friends:["Atham ","Jasur","Shahlo"]
 // }
 // console.log(userInfo);
+
+
+// const person = new Object();
+// person.name = "Sardorbek";
+// person.age = 25;
+// person.isDeveloper = true;
+
+// console.log(person);
+
+// const userInfo={
+//     firstName:"Sardorbek",
+//     lastName:"Sayfullayev",
+//     age:21,
+//     friends:["Atham ","Jasur","Shahlo"]
+// }
+// let name1="Name";
+
+// console.log(userInfo[`last${name1}`]);
+
+// let person = {
+//     name: "Sardorbek",
+//     age: 25,
+//     isDeveloper: true
+// };
+
+// console.log(person.age); 
+// console.log(person["age"]);
+
+// let person = {
+//     name: "Sardorbek",
+//     age: 25
+// };
+
+// person.isDeveloper = true;
+// person.secondName="Sayfullayev";
+
+// person.age = 26;
+// console.log(person);
+
+// let person = {
+//     name: "Sardorbek",
+//     age: 25,
+//     isDeveloper: true
+// };
+
+// delete person.age;
+// delete person.name;
+// delete person.isDeveloper;
+
+// console.log(person); 
+
+// const userInfo = {
+//     firstName: "Sardorbek",
+//     lastName: "Sayfullayev",
+//     age: 21,
+//     friends: ["Atham ", "Jasur", "Shahlo"]
+// }
+// let nomlangan = '';
+
+// for (let i = 0; i < userInfo.friends.length; i++) {
+//     if (userInfo.friends[i] !== undefined) {
+//         nomlangan += userInfo.friends[i];
+//     }
+// }
+// console.log(`Mening jami do'stlarimni soni ${userInfo.friends.length} va ularning ismlari ${nomlangan.split(" ").join("-")}`);
+

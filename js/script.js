bebe5b1f55a99052ef51a504c32fb4a7b2965565
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-03 14:24:54 +0500

    dddd

--- js/script.js
+++
@@ -1,9 +0,0 @@
-console.log("JavaScriptga xush kelibsiz!");
-console.log("JavaScriptga!");
-
-
-const array = [true, false, true, true];
-const result = array.reduce((acc, val) => log && val, true);
-
-console.log(result);
-

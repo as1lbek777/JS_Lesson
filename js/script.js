b633667fe4b24a19347d4f5d63b7561059f8ad6d
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-03 16:05:04 +0500

    node git add

--- js/script.js
+++ js/script.js
@@ -78,3 +78,67 @@
 // }
 // console.log(`Mening jami do'stlarimni soni ${userInfo.friends.length} va ularning ismlari ${nomlangan.split(" ").join("-")}`);
 
+
+// let person = {
+//     name: "Sardorbek",
+//     skills: ["HTML", "CSS", "JavaScript"],
+//     address: {
+//         city: "Tashkent",
+//         country: "Uzbekistan"
+//     }
+// };
+
+// console.log(person.skills[1]); 
+// console.log(person.address.city); 
+// let arr=[
+//     {
+//         firstName:"Nigora",
+//         secondName:"Kenjayeva",
+//         age:15,
+//         school:"26-maktab"
+//     },
+//     {
+//         firstName:"Jasmina",
+//         secondName:"Jo'rayeva",
+//         age:17,
+//         school:"5-maktab"
+//     },
+//     {
+//         firstName:"Bonur",
+//         secondName:"Bozorov",
+//         age:13,
+//         school:"95-maktab"
+//     }
+// ]
+// console.log(arr.map(index=>{
+//     return index.school
+// }));
+
+// const market = [
+//     {
+//         fruit: "olma",
+//         price: {
+//             kg: 1000,
+//             someonePrice: 2000
+//         }
+//     },
+//     {
+//         fruit: "nok",
+//         price: {
+//             kg: 500,
+//             someonePrice: 8000
+//         }
+//     },
+//     {
+//         fruit: "banan",
+//         price: {
+//             kg: 100,
+//             someonePrice: 6000
+//         }
+//     },
+// ]
+// let docWrt=market.map(index=>{
+//     return `Meva nomi:${index.fruit}\n Umumiy Narx:${index.price.kg*index.price.someonePrice} so'm`
+// })
+// document.write(docWrt)
+  

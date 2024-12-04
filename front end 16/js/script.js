d2db4a51caf4ec6b46c8d65c71a11cebc3581822
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-04 17:04:05 +0500

    add

--- front end 16/js/script.js
+++ front end 16/js/script.js
@@ -13,4 +13,35 @@
 // let name1=15
 // let x=first-name1;
 // console.log(x);
+// let age=15;
+// let age = 30; // Butun son
+// let price = 19.99; // Onlik son
+// console.log(typeof age);
 
+// let greeting = 15+5;
+// console.log(typeof greeting);
+
+// let isDeveloper=0;
+// console.log(typeof isDeveloper);
+
+
+// let myVariable;
+// console.log(typeof myVariable); 
+
+// let myVar = null; // Dasturchi tomonidan belgilandi
+// console.log(typeof myVar);
+
+
+// const uniqueSymbol = Symbol(true);
+// console.log(typeof uniqueSymbol);
+
+
+// const bigInt1 = 123456789012345678901234567890n; // n bilan tugatish
+// const bigInt2 = BigInt("123456789012345678901234567890"); // BigInt konstruktoridan foydalanish
+// console.log(typeof bigInt2);
+
+
+// let numberType=155n;
+// let bigIntType=155n;
+// let cost=numberType+bigIntType;
+// console.log(typeof cost);

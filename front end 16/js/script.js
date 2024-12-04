d0ccf62f6603ab7a52a920b0273e4d9f7a0c1ced
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-04 16:45:09 +0500

    adad

--- front end 16/js/script.js
+++ front end 16/js/script.js
@@ -1,3 +1,16 @@
-// let firstName="Sardorbek";
+//camel case usulida uzgaruvchilarni nomlash
+// let FirstName="Sardorbek";
 // let secondName="Sayfullayev";
 // let totalCost;
+
+//snake case usulida uzgaruvchilarni nomlash
+// let first_name="Sardorbek";
+// let second_name="Sayfullayev";
+// let total_cost;
+
+//kebab case usulida uzgaruvchilarni nomlash
+// let first=5;
+// let name1=15
+// let x=first-name1;
+// console.log(x);
+
